79713dd8186f4d686b0f875e3937befe7abdd16f
miguelfigueiredo <mbarbosaff@gmail.com>
2026-09-02 22:37:52 +0100

    chore: scaffold inicial do projeto (Spring Boot 2.6.1, pom.xml configurado)

---
+++ filmotokio/src/main/java/es/tokioschool/filmotokio/FilmotokioApplication.java
@@ -0,0 +1,13 @@
+package es.tokioschool.filmotokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotokioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmotokioApplication.class, args);
+	}
+
+}
---
+++ filmotokio/src/test/java/es/tokioschool/filmotokio/FilmotokioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.tokioschool.filmotokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmotokioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
